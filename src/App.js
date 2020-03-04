22bb127c9e8917b233691d35c547e6cb725b713c
GAURAV MISHRA <misra.gaurav789@gmail.com>
2020-03-05 02:19:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import CardList from "./cardList";
+import { robots } from "./robots";
+import SearchBox from "./searchBox";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+             robots : [],
+             searchField : ''
+        }
+    }
+
+    componentDidMount (){
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response=> {
+            return response.json();
+        })
+        .then(users=>{
+            this.setState({robots : users});
+        })
+    }
+    
+    onSearchChange = (event) => {
+        this.setState({ searchField : event.target.value });
+    }
+
+  render() {
+    const filteredRobots = this.state.robots.filter( robots => {
+        return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+    
+    return (
+        <div className="tc">
+          <h1> RoboFriends </h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+  }
+};
 
 export default App;
